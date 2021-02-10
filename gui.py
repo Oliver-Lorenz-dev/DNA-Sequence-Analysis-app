647d311b6a03b8cf0eeea99ce580961d9fcd7995
Oliver Lorenz <o.lorenz2@newcastle.ac.uk>
2021-02-10 19:17:51 +0000

    Changed output to GUI output (not CLI)

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import StringVar
 import tkinter.ttk as ttk
 from tkinter import simpledialog
 from tkinter import messagebox
@@ -12,6 +13,7 @@ class GUI(tk.Frame):
         # create a window
         super().__init__(master)
         self.window = master
+        self.text_out = tk.StringVar()
         # create a title
         title = tk.Label(self.window, text="Bioinformatics app")
         # add title to window
@@ -22,6 +24,10 @@ class GUI(tk.Frame):
         tk.Button(self.window, text="Open file", command=lambda: self.open_file()).pack()
         # create a GC button
         tk.Button(self.window, text="GC content", command=lambda: self.gc()).pack()
+        # create output label
+        out_label = tk.Label(self.window, textvariable=self.text_out)
+        # add out_label to window
+        out_label.pack()
         # wait for user input
         self.mainloop()
 
@@ -33,7 +39,7 @@ class GUI(tk.Frame):
             self.content = SeqIO.read(self.file, 'fasta')
         # tell user no file was opened if they press cancel
         except AttributeError:
-            print('You pressed cancel, no new file was opened')
+            self.text_out.set('You pressed cancel, no new file was opened')
             # note if a file has already been opened this will stay open
 
     def gc(self):
@@ -41,10 +47,11 @@ class GUI(tk.Frame):
         # check user has opened a file
         try:
             self.GC_content = GC(self.content.seq)
-            print('GC content:', "{:.2f}".format(self.GC_content) + '%')
+            self.text_out.set('GC content: ' + "{:.2f}".format(self.GC_content) + '%')
         # tell user to open a file
         except AttributeError:
-            print('Please open a FASTA file before using other functions of this application')
+            self.text_out.set('Please open a FASTA file before using other functions of this application')
+
 
 g = GUI()
 g
