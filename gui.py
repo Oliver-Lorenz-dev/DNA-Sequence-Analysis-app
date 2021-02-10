af4dd4351485b3c5e52f0d683a35e2af6ed73394
Oliver Lorenz <o.lorenz2@newcastle.ac.uk>
2021-02-10 18:39:27 +0000

    Added gc function.

--- gui.py
+++ gui.py
@@ -20,18 +20,31 @@ class GUI(tk.Frame):
         tk.Button(self.window, text="Quit", command=self.window.destroy).pack()
         # create open file button
         tk.Button(self.window, text="Open file", command=lambda: self.open_file()).pack()
+        # create a GC button
+        tk.Button(self.window, text="GC content", command=lambda: self.gc()).pack()
         # wait for user input
         self.mainloop()
 
     def open_file(self):
+        """function which opens a file"""
         self.file = askopenfile(mode='r+', filetypes=[('FASTA Files', '*.fasta'), ('FASTA files', '*.fa')])
         # check user has selected a file
         try:
             self.content = SeqIO.read(self.file, 'fasta')
         # tell user no file was opened if they press cancel
         except AttributeError:
-            print('You pressed cancel, no file was opened')
+            print('You pressed cancel, no new file was opened')
             # note if a file has already been opened this will stay open
+
+    def gc(self):
+        """function which determines the GC content of the sequence in a FASTA file"""
+        # check user has opened a file
+        try:
+            self.GC_content = GC(self.content.seq)
+            print('GC content:', "{:.2f}".format(self.GC_content) + '%')
+        # tell user to open a file
+        except AttributeError:
+            print('Please open a FASTA file before using other functions of this application')
 
 g = GUI()
 g
