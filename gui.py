a76e2da4dc1f27aa15e6554391ac039be45c993f
Oliver Lorenz <o.lorenz2@newcastle.ac.uk>
2021-02-11 15:23:11 +0000

    Added base_frequency function

--- gui.py
+++ gui.py
@@ -26,6 +26,8 @@ class GUI(tk.Frame):
         tk.Button(self.window, text="Open file", command=lambda: self.open_file()).pack()
         # create a GC button
         tk.Button(self.window, text="GC content", command=lambda: self.gc()).pack()
+        # create a base frequency button
+        tk.Button(self.window, text="Base frequency", command=lambda: self.base_frequency()).pack()
         # create output label
         out_label = tk.Label(self.window, textvariable=self.text_out)
         # add out_label to window
@@ -39,6 +41,8 @@ class GUI(tk.Frame):
         # check user has selected a file
         try:
             self.content = SeqIO.read(self.file, 'fasta')
+            # reset text_out
+            self.text_out.set('')
         # tell user no file was opened if they press cancel
         except AttributeError:
             self.text_out.set('You pressed cancel, no new file was opened')
@@ -54,6 +58,22 @@ class GUI(tk.Frame):
         except AttributeError:
             self.text_out.set('Please open a FASTA file before using other functions of this application')
 
+    def base_frequency(self):
+        """function which determines the percentage of each base in a DNA sequence"""
+        # check user has opened a file
+        try:
+            L = len(self.content.seq)
+            A = (self.content.seq.count('A') / L) * 100
+            G = (self.content.seq.count('G') / L) * 100
+            C = (self.content.seq.count('C') / L) * 100
+            T = (self.content.seq.count('T') / L) * 100
+            N = (self.content.seq.count('N') / L) * 100
+            self.text_out.set('A: ' + "{:.2f}".format(A) + '%  ' + 'G: ' + "{:.2f}".format(G) +
+                  '%  ' + 'C: ' + "{:.2f}".format(C) + '%  ' +
+                  'T: ' + "{:.2f}".format(T) + '%  ' + 'N: ' + "{:.2f}".format(N) + '%  ')
+        # tell user to open a file
+        except AttributeError:
+            self.text_out.set('Please open a FASTA file before using other functions of this application')
 
 g = GUI()
 g
